167962d33b6223b7cf0d88f0803d4d62b56e16d0
sharofiddin <pardayev.sharofiddin@gmail.com>
2022-08-14 13:44:35 +0500

    jpa tests

---
+++ ShopmeParent/ShopmeAdmin/src/test/java/com/shopme/admin/user/RoleRepositoryTests.java
@@ -0,0 +1,17 @@
+package com.shopme.admin.user;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class RoleRepositoryTests {
+    
+    @Autowired
+    private RoleRepository roleRepository;
+    @Test
+    void test() {
+	fail("Not yet implemented");
+    }
+
+}

e3ff60120fb0bead95438d07a4feca60b6724333
sharofiddin <pardayev.sharofiddin@gmail.com>
2022-08-17 00:19:14 +0500

    role test

--- ShopmeParent/ShopmeAdmin/src/test/java/com/shopme/admin/user/RoleRepositoryTests.java
+++ ShopmeParent/ShopmeAdmin/src/test/java/com/shopme/admin/user/RoleRepositoryTests.java
@@ -1,25 +1,41 @@
 package com.shopme.admin.user;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.stream.StreamSupport;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
 
 import com.shopme.common.entity.Role;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
 class RoleRepositoryTests {
     
     @Autowired
     private RoleRepository roleRepository;
     @Test
     void testCreateRole() {
-	Role role = roleRepository.save(new Role("admin", "Administrator"));
+	Role role = roleRepository.save(new Role("admin", "Manage everything"));
 	assertThat(role.getId()).isPositive();
+    }
+    
+    @Test
+    void testCreateRoles() {
+	Role salesPerson = roleRepository.save(new Role("salesperson", "Manage customer, product prices"));
+	Role editor = roleRepository.save(new Role("editor", "Manage products, menus, catalogs and brands"));
+	Role shipper = roleRepository.save(new Role("shipper", "View products and orders, change order status"));
+	Role assitant = roleRepository.save(new Role("assistant", "Manage questions and views"));
+	Iterable<Role> savedRoles = roleRepository.saveAll(List.of(salesPerson, editor, shipper, assitant));
+	assertEquals(4, StreamSupport.stream(savedRoles.spliterator(), false).count());
     }
 
 }
